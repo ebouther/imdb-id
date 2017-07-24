88c28037d951de608775c3452b0c4d17d9ebd99c
ebouther <ebouther@student.42.fr>
2017-07-24 15:11:26 +0200

    initial commit

---
+++ imdb-id.js
@@ -0,0 +1,27 @@
+var request = require("request");
+var cheerio = require('cheerio');
+
+function imdbId(title, cb) {
+  var url = "http://www.imdb.com/find?s=all&q=" + encodeURIComponent(title);
+  request(url, function (error, response, body) {
+      if (error) {
+        return cb (error, null);
+      }
+      if (response.statusCode !== 200) {
+        return cb("Imdb: HTTP bad response (Status Code: " + response.statusCode + ")");
+      }
+
+      let $ = cheerio.load(body);
+      let movie_link = $("#main > .article >  .findSection > .findList > tbody").find("td").find("a").attr('href');
+
+      if (movie_link) {
+        let regex = /\/title\/(t{2}\d+)\/?/;
+        let id = regex.exec(movie_link);
+        if (id) {
+          return cb(null, id[1]);
+        }
+      }
+
+      return cb("Movie not found", null);
+    });
+}
